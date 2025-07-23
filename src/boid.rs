1b1927d28723c1f3bf4d67671ab758e6ca3105cc
Torjus Åmellem Kallekleiv <torjus.kallekleiv@gmail.com>
2025-07-23 23:24:55 +0200

    Add boid behavior

--- src/boid.rs
+++ src/boid.rs
@@ -1,13 +1,11 @@
-mod constants;
-
-use constants::VIEW_RADIUS;
+use crate::constants::{AVOID_FACTOR, CENTERING_FACTOR, MATCHING_FACTOR, VIEW_RADIUS};
 
 #[derive(Clone)]
 pub struct Boid {
     x: f32,
     y: f32,
-    v_x: f32,
-    v_y: f32,
+    vx: f32,
+    vy: f32,
 }
 
 impl Boid {
@@ -15,8 +13,8 @@ impl Boid {
         Boid {
             x: 0.0,
             y: 0.0,
-            v_x: 0.0,
-            v_y: 0.0,
+            vx: 0.0,
+            vy: 0.0,
         }
     }
 
@@ -25,30 +23,71 @@ impl Boid {
         self.y += dy;
     }
 
-    pub fn update_velocity(&mut self, d_v_x: f32, d_v_y: f32) {
-        self.v_x += d_v_x;
-        self.v_y += d_v_y;
+    pub fn update_velocity(&mut self, d_vx: f32, d_vy: f32) {
+        self.vx += d_vx;
+        self.vy += d_vy;
     }
 
     pub fn distance_to(&self, neighbor: &Boid) -> f32 {
         let dx: f32 = neighbor.x - self.x;
         let dy: f32 = neighbor.y - self.y;
-        return dx * dx + dy * dy;
+        (dx * dx + dy * dy).sqrt()
     }
 
     pub fn is_visible(&self, neighbor: &Boid) -> bool {
         self.distance_to(neighbor) < VIEW_RADIUS
     }
 
-    pub fn separation(&self, neighbors: &[Boid]) -> (f32, f32) {
-        return (0.0, 0.0);
+    // Each boid attempts to avoid running into other boids. If two or more boids get too close to one another (i.e. within one another's protected range), they will steer away from one another. They will do so in the following way:
+    pub fn separation(&mut self, neighbors: &[Boid]) {
+        let mut close_dx: f32 = 0.0;
+        let mut close_dy: f32 = 0.0;
+
+        for neighbor in neighbors.iter() {
+            close_dx += self.x - neighbor.x;
+            close_dy += self.y - neighbor.y;
+        }
+
+        self.update_velocity(close_dx * AVOID_FACTOR, close_dy * AVOID_FACTOR);
     }
 
-    pub fn alignment(&self, neighbors: &[Boid]) -> (f32, f32) {
-        return (0.0, 0.0);
+    //Each boid attempts to match the velocity of other boids inside its visible range. It does so in the following way:
+    pub fn alignment(&mut self, neighbors: &[Boid]) {
+        let mut xvel_avg: f32 = 0.0;
+        let mut yvel_avg: f32 = 0.0;
+        let neighboring_boids: f32 = neighbors.len() as f32;
+
+        for neighbor in neighbors.iter() {
+            xvel_avg += neighbor.vx;
+            yvel_avg += neighbor.vy;
+        }
+
+        xvel_avg = xvel_avg / neighboring_boids;
+        yvel_avg = yvel_avg / neighboring_boids;
+
+        self.update_velocity(
+            (xvel_avg - self.vx) * MATCHING_FACTOR,
+            (yvel_avg - self.vy) * MATCHING_FACTOR,
+        );
     }
 
-    pub fn cohesion(&self, neighbors: &[Boid]) -> (f32, f32) {
-        return (0.0, 0.0);
+    // Each boid steers gently toward the center of mass of other boids within its visible range. It does so in the following way:
+    pub fn cohesion(&mut self, neighbors: &[Boid]) {
+        let mut xpos_avg: f32 = 0.0;
+        let mut ypos_avg: f32 = 0.0;
+        let neighboring_boids: f32 = neighbors.len() as f32;
+
+        for neighbor in neighbors.iter() {
+            xpos_avg += neighbor.x;
+            ypos_avg += neighbor.y;
+        }
+
+        xpos_avg = xpos_avg / neighboring_boids;
+        ypos_avg = ypos_avg / neighboring_boids;
+
+        self.update_velocity(
+            (xpos_avg - self.x) * CENTERING_FACTOR,
+            (ypos_avg - self.y) * CENTERING_FACTOR,
+        );
     }
 }
--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,6 @@
 pub const NUM_BOIDS: usize = 100;
 pub const MAX_SPEED: f32 = 5.0;
 pub const VIEW_RADIUS: f32 = 50.0;
+pub const AVOID_FACTOR: f32 = 1.0;
+pub const MATCHING_FACTOR: f32 = 1.0;
+pub const CENTERING_FACTOR: f32 = 1.0;
