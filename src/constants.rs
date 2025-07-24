bc18d57b022d17802c616ead95459e1a6de854b0
Torjus Åmellem Kallekleiv <torjus.kallekleiv@gmail.com>
2025-07-24 23:29:04 +0200

    Update constants

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,6 @@
 pub const NUM_BOIDS: usize = 100;
-pub const MAX_SPEED: f32 = 5.0;
+pub const MAX_SPEED: f32 = 2.0;
 pub const VIEW_RADIUS: f32 = 50.0;
-pub const AVOID_FACTOR: f32 = 1.0;
-pub const MATCHING_FACTOR: f32 = 1.0;
-pub const CENTERING_FACTOR: f32 = 1.0;
+pub const AVOID_FACTOR: f32 = 0.05;
+pub const MATCHING_FACTOR: f32 = 0.05;
+pub const CENTERING_FACTOR: f32 = 0.01;
